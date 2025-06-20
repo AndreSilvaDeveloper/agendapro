07314e598ea9f2f0b264a9895a493c2914134749
AndreSilvaDeveloper <andre.admft@gmail.com>
2025-06-20 11:21:56 -0300

    subindo codigo

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 const app = express();
 
-// Conexão com MongoDB
-mongoose.connect('mongodb+srv://Andresamara93:Andresamara93@devhouse.owgtbeh.mongodb.net/salon_agenda?retryWrites=true&w=majority&appName=devhouse')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("🟢 Conectado ao MongoDB"))
-  .catch(err => console.error("🔴 Erro na conexão com MongoDB:", err));
+  .catch(err => console.error("🔴 Erro ao conectar MongoDB:", err));
 
-// Sessão (login)
 app.use(session({
   secret: 'salao-kadosh-segredo',
   resave: false,
   saveUninitialized: true
 }));
 
-// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-// Rotas
 app.use('/', routes);
 
-// Porta dinâmica para deploy
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
