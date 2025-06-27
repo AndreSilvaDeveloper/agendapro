cff1657a6ffb919df8fb734f09a480f376e6be37
AndreSilvaDeveloper <andre.admft@gmail.com>
2025-06-27 17:42:49 -0300

    Atualizando

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 require('dotenv').config();
-const express    = require('express');
-const mongoose   = require('mongoose');
-const session    = require('express-session');
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const routes     = require('./routes/index');
+const routes = require('./routes/index');
+express.static('public')
 
 const app = express();
 
-// Conexão ao MongoDB com timeout maior
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 60000   // aguarda até 60 segundos por um primário
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("🟢 Conectado ao MongoDB"))
   .catch(err => console.error("🔴 Erro ao conectar MongoDB:", err));
 
-// Middlewares
 app.use(session({
   secret: 'salao-kadosh-segredo',
   resave: false,
   saveUninitialized: true
 }));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
-
-// Static files
 app.use(express.static('public'));
-
-// Rotas
 app.use('/', routes);
 
-// Inicialização do servidor
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
